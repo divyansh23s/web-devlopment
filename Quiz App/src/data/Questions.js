490d847159bfc97e53ff7d91a85f49f969557c17
divyansh23s <singhdivyansh0890@gmail.com>
2025-06-30 21:33:38 +0530

    Add files via upload

---
+++ Quiz App/src/data/Questions.js
@@ -0,0 +1,44 @@
+const questions = [
+  {
+    question: "What is the capital of France?",
+    options: ["London", "Berlin", "Paris", "Madrid"],
+    answer: "Paris",
+  },
+  {
+    question: "Which language is used for web development?",
+    options: ["Python", "JavaScript", "C", "Ruby"],
+    answer: "JavaScript",
+  },
+  {
+    question: "What is 2 + 2?",
+    options: ["3", "4", "22", "5"],
+    answer: "4",
+  },
+  {
+    question: "React is a ______?",
+    options: ["Framework", "Library", "Database", "Language"],
+    answer: "Library",
+  },
+  {
+    question: "What does CSS stand for?",
+    options: [
+      "Computer Style Sheets",
+      "Creative Style System",
+      "Cascading Style Sheets",
+      "Colorful Style Sheets",
+    ],
+    answer: "Cascading Style Sheets",
+  },
+  {
+    question: "Who developed React.js?",
+    options: ["Google", "Microsoft", "Meta", "Amazon"],
+    answer: "Meta",
+  },
+  {
+    question: "Which tag is used to define JavaScript?",
+    options: ["<script>", "<js>", "<javascript>", "<code>"],
+    answer: "<script>",
+  },
+];
+
+export default questions;
